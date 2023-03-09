2890184cb80a3cd619094736e1e4856f4a397c0d
Brian <polobrallardbrian@gmail.com>
2023-03-09 01:33:22 -0300

    added js exercises

---
+++ github-profiles/brianbr-3719/index.js
@@ -0,0 +1,14 @@
+
+// ejercicios
+if(total === 85){
+    console.log("hugo: " + hugo)
+    console.log("paco: " + paco)
+    console.log("luis: " + luis)
+    
+  }
+  
+let a = []
+  
+if (typeof a === "object"){
+    console.log("a es un objeto")
+  }  
