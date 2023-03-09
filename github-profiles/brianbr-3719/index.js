c0f5b5bbc73bc1d4f422565cbf08e48aa06006bc
Brian <polobrallardbrian@gmail.com>
2023-03-09 18:25:25 -0300

    fixed index.js

--- github-profiles/brianbr-3719/index.js
+++ github-profiles/brianbr-3719/index.js
@@ -1,5 +1,13 @@
 
 // ejercicios
+//asignamos la cantidad de monedas de Hugo
+let hugo = 15
+// Calculamos la cantidad de monedas de Paco y Luis en funcion de hugo
+let paco = 2 * hugo
+let luis = paco + 10
+//sumamos las cantidades de monedas de los tres amigos
+let total = hugo + paco + luis
+//y agregamos el condicional que solamente se ejecutara con el valor correcto de hugo (15).
 if(total === 85){
     console.log("hugo: " + hugo)
     console.log("paco: " + paco)
@@ -9,6 +17,8 @@ if(total === 85){
   
 let a = []
   
-if (typeof a === "object"){
+if (typeof a === "object" && a !== null){
     console.log("a es un objeto")
-  }  
+  } else {
+    console.log("a no es un objeto")
+  }
