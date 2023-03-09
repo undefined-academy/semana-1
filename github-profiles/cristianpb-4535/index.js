8ad9565e0d8f781af5bce7b8462b482cdd0acf8f
cristianpolo <axelpolo9927@gmail.com>
2023-03-09 01:59:26 -0300

    agregue ejercicio de js

---
+++ github-profiles/cristianpb-4535/index.js
@@ -0,0 +1,14 @@
+let hugo = 15
+let paco = hugo * 2
+let luis = paco + 10
+let total = hugo + paco + luis
+if(total===85){
+    console.log("hugo:"+ hugo)
+    console.log("paco:"+ paco)
+    console.log("luis:"+ luis)
+
+}
+let a = null
+if(typeof a==="object"){
+    console.log("a es un objeto")
+}
