9d0d3c17ec4bea3c93cee51d0d9e3339d44a6dd6
Mauro Favre <mauro_favre@hotmail.com>
2023-03-10 00:21:07 -0300

    Ejercicios-clase-5

---
+++ maurofavre-3566/script.js
@@ -0,0 +1,37 @@
+const human = {
+    age:32,
+    apellido:"favre",
+     nombre:"mauro",
+}
+
+// ----------------------------------------------------------------
+// Ejercicios clases 5.
+// Hugo, Paco y Luis tienen una cantidad
+// desconocida de monedas cada uno.
+// Sabemos que Paco tiene el doble de
+// monedas que Hugo y que Luis tiene 10
+// monedas más que Paco.
+// Si los tres juntos tienen un total de 85
+// monedas.
+// ¿Cuántas monedas tiene
+// cada uno?
+
+
+// let hugo = 15;
+// let paco = 2 * hugo
+// let luis = paco + 10
+
+// let total= hugo+ paco + luis;
+
+// if (total === 85) {
+//     console.log({hugo, paco, luis });  
+// }
+// ---------------------------------
+// ¿Cómo puedo implementar una expresión para
+// verificar si un valor es un objeto?
+
+// typeof obj === "object" && obj != null
+
+
+
+
