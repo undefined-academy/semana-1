afaa572588b0b3976704e43f7907a09e510ff09d
José F <josefcontrerasa@gmail.com>
2023-03-12 12:06:56 -0400

    At this time, I'll add some exercises and some JavaScript content

---
+++ second_exercise.js
@@ -0,0 +1,13 @@
+const human = {
+    name: "José",
+    lastname: "Contreras",
+    hair_color: "Brown",
+    skin_color: "Soft brown",
+    age: 27,
+    singer: false,
+    marriage: false,
+};
+
+// the way to identificate an Array is asking with [Array].isArray = true/false
+// for example: console.log(Array.isArray([1, 2, 3]));
+// answer = true
