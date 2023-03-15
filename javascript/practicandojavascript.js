14f55d75f88eab0e799a7ed8393b0a01fe0ff3e7
Irene Alvarez <irenealbarez@gmail.com>
2023-03-15 20:48:24 +0100

    Cambios HTML semántico

---
+++ javascript/practicandojavascript.js
@@ -0,0 +1,14 @@
+let hugo = 0;
+/*Calculamos la cantidad de
+monedas de Paco y Luis en función
+de Hugo*/
+let paco = 2 * hugo;
+let luis = paco + 10;
+/* Sumamos las cantidades de
+monedas de los tres amigos*/
+let total = hugo + paco + luis;
+if (total === 85) {
+ console.log("Hugo: " + hugo)
+ console.log("Paco: " + paco)
+ console.log("Luis: " + luis)
+}
