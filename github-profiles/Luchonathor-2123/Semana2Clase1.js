0eb37951c180e7d0b37ee5acc3e3212f1f8264dc
root <root@LAPTOP-9AGNLD66>
2023-03-14 22:22:39 -0500

    Variacion ejercicio Hugo Paco y Luis

---
+++ github-profiles/Luchonathor-2123/Semana2Clase1.js
@@ -0,0 +1,15 @@
+let total = hugo + paco + luis;
+let hugo = 0;
+let paco = 0;
+let luis = 0;
+do {
+    hugo++;
+    paco = 2 * hugo;
+    luis = paco + 10;
+    total = hugo + paco + luis;
+} while (total !== 85);
+
+console.log(`Hugo - ${ hugo }`);
+console.log(`Paco - ${ paco }`);
+console.log(`Luis - ${ luis }`);
+
